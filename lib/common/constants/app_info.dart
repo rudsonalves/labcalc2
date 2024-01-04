97aa972128704275ac4c94c6314f464d2a9caaeb
rudsonalves <rudsonalves67@gmail.com>
2024-01-04 13:55:33 -0300

    version: 1.0.5+18:
    This update enhances the functionality related to accessing external resources like the Privacy Policy page and streamlines communication features within the app. Key changes include:
    
    * android/app/src/main/AndroidManifest.xml:
      - Added permissions to access internet pages through the browser. This is used to access the Privacy Policy page.
    * lib/common/constants/app_info.dart:
      - Adjusted the AppInfo to include new methods:
        - static Future<void> launchUrl(String url) for opening a URL in the browser.
        - static Future<void> copyUrl(String url) to copy a URL to the clipboard.
        - static Future<void> launchMailto() for email communication.
    * lib/features/about/about_page.dart:
      - The Privacy Policy page link now opens in the browser for direct access.
    * lib/features/calculator/widgets/update_message/update_message.dart:
      - Updated the message content.
      - Added a button linking to the Privacy Policy for easy access.

--- lib/common/constants/app_info.dart
+++ lib/common/constants/app_info.dart
@@ -1,7 +1,44 @@
+import 'dart:developer';
+
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart' as launcher;
+
 class AppInfo {
   AppInfo._();
 
   static const name = 'labcalc2';
   static const description = 'Laboratory Scientific Calculator';
-  static const version = '1.0.5+17';
+  static const version = '1.0.5+18';
+
+  static get pageUrl => 'https://jrblog.com.br/$name/';
+  static const email = 'alvesdev67@gmail.com';
+  static const privacyPolicyUrl = 'https://jrblog.com.br/privacy-policy-en/';
+  
+  static Future<void> launchUrl(String url) async {
+    final uri = Uri.parse(url);
+
+    // copy link to clipboard
+    await copyUrl(url);
+
+    // open link in browser
+    if (await launcher.canLaunchUrl(uri)) {
+      await launcher.launchUrl(uri);
+    } else {
+      log("URL can't be launched.");
+    }
+  }
+
+  static Future<void> copyUrl(String url) async {
+    // copy link to clipboard
+    await Clipboard.setData(ClipboardData(text: url));
+  }
+
+  static Future<void> launchMailto() async {
+    Uri url = Uri(
+      scheme: 'mailto',
+      path: email,
+      queryParameters: {'subject': '[] - <Your Subject text>'},
+    );
+    await launcher.launchUrl(url);
+  }
 }
